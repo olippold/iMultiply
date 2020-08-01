781ff5137f36275e1550b3adac05770ddfe19eb5
Oliver Lippold <olippold@hotmail.com>
2020-08-01 14:58:40 +0100

    Initial Commit

---
+++ iMultiply/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  iMultiply
+//
+//  Created by Oliver Lippold on 01/08/2020.
+//  Copyright © 2020 Oliver Lippold. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
